bb11c527e08e21564dfe4a1462ce2961ed5dcd09
William Menten-Weil <wtmetnen@gmail.com>
2016-01-18 17:08:59 -0800

    testing ssh keys

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 /*
 |--------------------------------------------------------------------------
@@ -25,7 +25,54 @@ Route::get('/', function () {
 | kernel and includes session state, CSRF protection, and more.
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::auth();
 
 Route::group(['middleware' => ['web']], function () {
-    //
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/tasks', 'TaskController@index');
+    Route::post('/task', 'TaskController@store');
+    Route::delete('/task/{task}', 'TaskController@destroy');
+
+    //basic tutorial
+    /*
+    Route::get('/', function () {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    });
+
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    Route::delete('/task/{task}', function (Task $task) {
+        $task->delete();
+
+        return redirect('/');
+    });
+    */
+
 });
